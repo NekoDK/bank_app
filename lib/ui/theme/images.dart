8ef89deda873446c62d8c09d1a8182adbbe7f671
NekoDK <koldim14kol@gmail.com>
2023-12-07 15:50:20 +0600

    add images

---
+++ lib/ui/theme/images.dart
@@ -0,0 +1,11 @@
+class ImageAssets {
+  static String arrowIcon = 'assets/ic_arrow.svg';
+  static String closeIcon = 'assets/ic_close.svg';
+  static String exitIcon = 'assets/ic_exit.svg';
+  static String limitIcon = 'assets/ic_limit.svg';
+  static String percentIcon = 'assets/ic_percent.svg';
+  static String roundPercentIcon = 'assets/ic_round_percent.svg';
+  static String sberIcon = 'assets/ic_sber.svg';
+  static String transferIcon = 'assets/ic_transfer.svg';
+  static String userImage = 'assets/ic_user.webp';
+}
